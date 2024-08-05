6c3745248957716328a5c660ec6ef65028c98002
Mythical-Github <MythicalData@gmail.com>
2024-08-05 18:23:48 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,17 +5,17 @@ setup(
     version='0.1',
     packages=find_packages(),
     install_requires=[
-        'git+https://github.com/Mythical-Github/python_logging.git',
+        'python_logging @ git+https://github.com/Mythical-Github/python_logging.git',
     ],
     include_package_data=True,
     package_data={
         '': ['cli.json.example'],
     },
-    description='A Python module with various cli functionality',
+    description='A Python module with various cli functionality.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/Mythical-Github/cli_py',
     author='Mythical',
     author_email='mythicaldata.com',
-    license='GPL3',
+    license='GPL-3.0',
 )
